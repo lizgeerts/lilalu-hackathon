290425a31d0c3271086a8038daf2247aa82f9a68
geertsliz <liz.geerts@student.howest.be>
2024-11-04 11:12:14 +0100

    room2 finished

---
+++ room2.js
@@ -0,0 +1,10 @@
+const grid = document.querySelector(".grid");
+const number = document.querySelector(".number");
+
+const randomIndex = Math.floor(Math.random() * 9);
+
+const row = Math.floor(randomIndex / 3) + 1;
+const col = (randomIndex % 3) + 1;
+
+number.style.gridRow = row;
+number.style.gridColumn = col;
