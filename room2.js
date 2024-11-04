cf3cb943195ff82e32b7e7e3794721a36e42731c
geertsliz <liz.geerts@student.howest.be>
2024-11-04 11:26:47 +0100

    room 2 finish

--- room2.js
+++ room2.js
@@ -3,8 +3,8 @@ const number = document.querySelector(".number");
 
 const randomIndex = Math.floor(Math.random() * 9);
 
-const row = Math.floor(randomIndex / 3) + 1;
-const col = (randomIndex % 3) + 1;
+const row = Math.floor(randomIndex / 3) + 1; // + 1 bc index starts at 0
+const col = (randomIndex % 3) + 1; 
 
 number.style.gridRow = row;
 number.style.gridColumn = col;
